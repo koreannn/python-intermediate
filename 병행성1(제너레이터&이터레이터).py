0520a757ff4a2b10dce19905fd11f48e3d19f97c
koreannn <ghdtjdwo5@gamil.com>
2025-05-01 15:04:41 +0900

    주석제거

--- 병행성1(제너레이터&이터레이터).py
+++ 병행성1(제너레이터&이터레이터).py
@@ -6,17 +6,12 @@
 - 클래스 기반으로 제너레이터 구현하기
 """
 
-# 제너레이터: Iterator를 만들어내는것
-# 파이썬에서 반복 가능(Iterable)한것들: collections에 있는 모든것들 / 텍스트 파일 / 리스트, 딕셔너리, 셋, 튜플 / unpacking한 요소들 / *args, **kwargs ...
-# Iterator는 반복 가능한것을 만들어내는 것
-
+"""Iterable객체가 반복 가능한 이유"""
 t = "ASDFGHJKL"
 print(dir(t))
 
 for char in t:
     print(char)
-# 반복 가능한 이유: 내부적으로 __iter__를 호출할 수 있는 객체이므로
-# 원리: t의 __iter__메서드가 호출되어 반복 가능한 객체를 만들어내고, 각 반복마다 __next__메서드가 호출되어 반복되는 요소를 반환
 
 w = iter(t)
 print("w: ", w)
@@ -43,9 +38,9 @@ print("hasattr(t, '__iter__'): ", hasattr(t, "__iter__")) # "t객체가 __iter__
 from collections import abc # abstract base class (추상 클래스)
 print("isinstance(t, abc.Iterable): ", isinstance(t, abc.Iterable)) # "t객체가 abc.Iterable 클래스의 인스턴스임?"
 
+print()
+print()
 
-print()
-print()
 
 """클래스로 제너레이터 구현하기"""
 # 1. Next Pattern
@@ -83,10 +78,6 @@ print()
 
 
 # 2. Generator Pattern
-# 1) 지능형 리스트, 딕셔너리, 집합 -> 데이터 양 증가로 인한 메모리 사용량 증가 -> 제너레이터 사용 권장
-# 2) 단위 실행 가능한 코루틴(Coroutine) 작성 가능
-# 3) 작은 메모리 조각들을 병렬로 실행 가능(즉, 작은 메모리 조각들을 병렬로 실행 가능)
-
 class WordSplitGenerator:
     def __init__(self, text):
         self._text = text.split(" ") # 인덱스값 필요없음
